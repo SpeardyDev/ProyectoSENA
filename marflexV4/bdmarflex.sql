de3463654adbadb95777e90dab8057387beeefcd
Reynaldo Martínez Fuentes <129266259+ReyMartinez2002@users.noreply.github.com>
2025-05-19 22:51:22 -0500

    BD actualizada

--- marflexV4/bdmarflex.sql
+++ marflexV4/bdmarflex.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-04-2025 a las 02:34:34
+-- Tiempo de generación: 20-05-2025 a las 05:50:16
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -198,8 +198,7 @@ CREATE TABLE `colchones` (
 INSERT INTO `colchones` (`ID`, `Modelo`, `Descripcion`, `Fecha_Fabricacion`, `Cantidad`) VALUES
 (1, 'Colchón Ortopédico Premium', 'Colchón con espuma HR y resortes Bonnell', '2025-03-07', 10),
 (2, 'Colchón Viscoelástico Deluxe', 'Colchón con espuma viscoelástica y forro Jacquard', '2025-03-07', 5),
-(3, 'Colchón Básico Espuma', 'Colchón económico solo de espuma HR', '2025-03-06', 15),
-(8, 'ss', 'ssssssss', '2025-04-11', 50);
+(3, 'Colchón Básico Espuma', 'Colchón económico solo de espuma HR', '2025-03-06', 15);
 
 -- --------------------------------------------------------
 
@@ -266,10 +265,10 @@ CREATE TABLE `materia_prima` (
 --
 
 INSERT INTO `materia_prima` (`ID`, `Nombre`, `Descripcion`, `Stock`, `Unidad`) VALUES
-(1, 'Espuma HR 30', 'Espuma de alta resiliencia para colchones', 265, 'kg'),
-(2, 'Tela Jacquard', 'Tela premium para forro de colchones', 190, 'metros'),
-(3, 'Resortes Bonnell', 'Resortes de acero para colchón ortopédico', 50, 'unidades'),
-(4, 'Pegamento PU', 'Pegamento de poliuretano para colchones', 199, 'litros');
+(1, 'Espuma HR 30', 'Espuma de alta resiliencia para colchones', 224, 'kg'),
+(2, 'Tela Jacquard', 'Tela premium para forro de colchones', 170, 'metros'),
+(3, 'Resortes Bonnell', 'Resortes de acero para colchón ortopédico', 30, 'unidades'),
+(4, 'Pegamento PU', 'Pegamento de poliuretano para colchones', 179, 'litros');
 
 -- --------------------------------------------------------
 
@@ -362,36 +361,42 @@ CREATE TABLE `solicitudes_materia_prima` (
 --
 
 INSERT INTO `solicitudes_materia_prima` (`ID`, `ID_Usuario`, `ID_MateriaPrima`, `Cantidad_Solicitada`, `Fecha_Solicitud`, `Estado`, `Motivo_Rechazo`) VALUES
-(29, 1, 1, 20, '2025-04-18 22:13:56', 'Aprobada', ''),
-(30, 1, 4, 222, '2025-04-18 22:21:49', 'Rechazada', 'No stock'),
-(31, 2, 2, 50, '2025-04-18 22:24:06', 'Pendiente', ''),
-(32, 2, 3, 20, '2025-04-18 23:24:40', 'Pendiente', ''),
-(36, 4, 4, 300, '2025-04-19 00:06:25', 'Pendiente', '');
+(1, 1, 4, 20, '2025-05-20 01:45:40', 'Aprobada', NULL),
+(39, 1, 2, 20, '2025-05-20 02:27:40', 'Aprobada', ''),
+(40, 1, 1, 41, '2025-05-20 03:07:57', 'Aprobada', ''),
+(41, 9, 1, 20, '2025-05-20 03:43:11', 'Rechazada', 'Nada'),
+(42, 12, 3, 200, '2025-05-20 03:47:35', 'Pendiente', '');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `users`
 --
 
-CREATE TABLE `usuarios` (
-  `ID` int(11) NOT NULL,
-  `Nombre` varchar(100) NOT NULL,
-  `Usuario` varchar(50) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `Rol` enum('Adminitrador','Empleado') NOT NULL DEFAULT 'Empleado',
-  `ID_Estado` tinyint(4) NOT NULL DEFAULT 1,
-  `FotoPerfil` varchar(255) DEFAULT NULL
+CREATE TABLE `users` (
+  `id` int(24) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` varchar(50) DEFAULT 'Empleado',
+  `documento` varchar(30) DEFAULT NULL,
+  `ID_Estado` tinyint(4) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
+  `fotoPerfil` varchar(255) DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `usuarios` (`ID`, `Nombre`, `Usuario`, `Password`, `Rol`, `ID_Estado`, `FotoPerfil`) VALUES
-(1, 'Nicolas Mahecha Martínez', 'nicolasmahechamartinez@gmail.com', '', 'Empleado', 1, 'foto-perfil.jpg'),
-(2, 'Paula Andrea Garcia Cifuentes', 'andrea.paula0127@gmail.com', '', 'Empleado', 1, '1745016963902.jpg'),
-(4, 'Ivan Pérez Castañeda', 'ivanalejandroperez2005@gmail.com', '', 'Empleado', 1, '1745021072565.jpg');
+INSERT INTO `users` (`id`, `username`, `password`, `rol`, `documento`, `ID_Estado`, `nombre`, `telefono`, `fotoPerfil`, `createdAt`, `updatedAt`) VALUES
+(1, 'ivanalejandroperez2005@gmail.com', '$2b$10$h.beaDtz3t45hP0Kybge.ulDZW/Ga8e4aWPsisk0QCMz5muSrdm4u', 'Empleado', '987654321', 1, 'Ivan Pérez Castañeda', '(312) 351-0076', '1745021072565.jpg', '2025-05-20 01:35:43', '2025-05-20 03:45:07'),
+(2, 'frbsfrbs1@gmail.com', '$2b$10$9mr4oXvOV1VYVA15PXEyGuGR5vYsXOSjkaxV97NXrn1w3DEMTmaNO', 'Administrador', '103456789', 1, 'Reynaldo Martínez Fuentes', '(310) 350-4442', '1744070269169.jpg', '2025-05-20 01:36:49', '2025-05-20 01:36:49'),
+(9, 'andrea.paula0127@gmail.com', '$2b$10$RzoK6b6eeZE25uBEBM7Eze3BxOQ4uFHNkUsjdYqit/U57/hpZ5pny', 'Empleado', '456123789', 2, 'Paula Andrea Garcia Cifuentes', '(311) 534-5560', '1745016963902.jpg', '2025-05-20 03:20:42', '2025-05-20 03:44:09'),
+(11, 'sandritha1000@gmail.com', '$2b$10$k2D2FxzRVTmIokdZ0jrJk.6nIkiVaPjjeUAtwe5b5ufYAT2KGE0WW', 'Administrador', '321987654', 1, 'Sandra Viviana Ruiz', '(315) 049-9050', '1743896358402.jpg', '2025-05-20 03:34:14', '2025-05-20 03:44:43'),
+(12, 'nicolasmahechamartinez@gmail.com', '$2b$10$QMcwM/F8572saEpFkee56.He7UO08fq2.Pqe2wzQKISTlvEbg66Z6', 'Empleado', '789123456', 1, 'Nicolas Mahecha Martínez', '(302) 315-6653', '1747712846223.jpg', '2025-05-20 03:35:33', '2025-05-20 03:47:26');
 
 --
 -- Índices para tablas volcadas
@@ -443,16 +448,16 @@ ALTER TABLE `proveedores`
 ALTER TABLE `solicitudes_materia_prima`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `id_materia_prima` (`ID_MateriaPrima`),
-  ADD KEY `solicitudes_materia_prima_ibfk_1` (`ID_Usuario`);
+  ADD KEY `solicitudes_materia_prima_ibfk_1` (`ID_Usuario`),
+  ADD KEY `ID_Usuario` (`ID_Usuario`);
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla `users`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `usuario` (`Usuario`),
-  ADD UNIQUE KEY `Nombre` (`Nombre`),
-  ADD KEY `estado_id` (`ID_Estado`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `ID_Estado` (`ID_Estado`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -492,13 +497,13 @@ ALTER TABLE `proveedores`
 -- AUTO_INCREMENT de la tabla `solicitudes_materia_prima`
 --
 ALTER TABLE `solicitudes_materia_prima`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT de la tabla `users`
 --
-ALTER TABLE `usuarios`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `users`
+  MODIFY `id` int(24) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restricciones para tablas volcadas
@@ -523,13 +528,13 @@ ALTER TABLE `movimientos`
 --
 ALTER TABLE `solicitudes_materia_prima`
   ADD CONSTRAINT `solicitudes_materia_prima_ibfk_1` FOREIGN KEY (`ID_MateriaPrima`) REFERENCES `materia_prima` (`ID`),
-  ADD CONSTRAINT `solicitudes_materia_prima_ibfk_2` FOREIGN KEY (`ID_Usuario`) REFERENCES `usuarios` (`ID`);
+  ADD CONSTRAINT `solicitudes_materia_prima_ibfk_3` FOREIGN KEY (`ID_Usuario`) REFERENCES `users` (`id`);
 
 --
--- Filtros para la tabla `usuarios`
+-- Filtros para la tabla `users`
 --
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`ID_Estado`) REFERENCES `estados` (`ID`);
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`ID_Estado`) REFERENCES `estados` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
